c4d3774411cf3c385fb7bd62166f4f0599c40e59
Mats van Es <m.vanes@donders.ru.nl>
2018-11-02 11:52:53 +0100

    bugfix

--- matlab/fourier2pow.m
+++ matlab/fourier2pow.m
@@ -1,20 +1,31 @@
-function pow = fourier2pow(filter, fourierspctrm, cumtapcnt)
-% this function creates power values in source space from the combination
-% of the fourierspectrum and the spatial filter. The input cumtapcnt
-% contains information about amount of tapers per trial.
+function pow = fourier2pow(w,fourier,tapvec)
 
-nrpt = numel(cumtapcnt);
-ntap_all = sum(cumtapcnt);
+siz = [size(w) 1];
+nchan = siz(2);
+ndip  = siz(3);
+nrpt  = numel(tapvec);
+ntap  = size(fourier,1);
 
-ix = [];
-iz = [];
-for k=1:nrpt
-    ntap = cumtapcnt(k);
-    ix = [ix; repmat(k, [ntap 1])];
-    iz = [iz; ones(ntap,1)./ntap];
+if nchan~=size(fourier,2)
+  error('unexpected dimensions');
 end
-iy = 1:ntap_all;
 
-P  = sparse(iy,ix,iz,ntap_all,nrpt);
+% create the projection matrix from tapers to trials
+ix = (1:ntap)';
+iy = zeros(ntap,1);
+iz = zeros(ntap,1);
+indx = 0;
+for k = 1:numel(tapvec)
+  indx = max(indx)+(1:tapvec(k));
+  iy(indx) = k;
+  iz(indx) = 1./tapvec(k);
+end
+P = sparse(ix,iy,iz);
 
-pow = (abs(filter*transpose(fourierspctrm)).^2)*P;
+w = permute(w, [3 2 1]);
+pow = zeros(size(w,1), nrpt);
+fourier = transpose(fourier);
+for k = 1:size(w,3)
+  % sum all components of w
+  pow = pow + (abs(w(:,:,k)*fourier).^2)*P;
+end
