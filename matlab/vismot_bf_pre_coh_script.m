170d1b673170371cb1c90a3682a24c1f754219ce
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2018-11-02 09:51:26 +0100

    ENH - added script for power computation, renamed the old one

---
+++ matlab/vismot_bf_pre_coh_script.m
@@ -0,0 +1,23 @@
+% this script performs spectral analysis of a given subject, using pre-computed
+% data, and divides pre and post cue onset intervals per condition
+
+if ~exist('frequency', 'var')
+    error('frequency should be defined');
+end
+
+if ~exist('nrand', 'var')
+  nrand = 100;
+end
+
+if ~exist('subjectname', 'var')
+    error('subjectname needs to be defined');
+end
+if ~exist('smoothing', 'var')
+    smoothing = 4;
+end
+subject = vismot_subjinfo(subjectname);
+load(fullfile(subject.pathname,'grid',sprintf('%s_sourcemodel3d8mm',subject.name)),'sourcemodel');
+[coh,zx13,zx42,looptime] = vismot_bf_pre_coh(subject,'sourcemodel',sourcemodel,'frequency',frequency,'smoothing',smoothing,'nrand',nrand);
+
+filename = fullfile(subject.pathname,'source',[subject.name,'coh6d8mm_',num2str(frequency)]);
+save(filename, 'zx13', 'zx42');
