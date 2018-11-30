0dc24dfe6dded0044a5b1ad8da40333c3347580d
Mats van Es <m.vanes@donders.ru.nl>
2018-11-30 10:07:05 +0100

    add options for conditioning on current_previous

--- matlab/vismot_script_Tstat_pre.m
+++ matlab/vismot_script_Tstat_pre.m
@@ -1,14 +1,25 @@
 datadir = '/project/3011085.03/analysis/source';
+conditions = 'current_previous'; % 'previous' current_previous
+if strcmp(conditions, 'current_previous')
+    whichstat = 'statIC_ICvsC'; % statC_CvsN statIC_ICvsC statIC_ICvsN statC_CvsIC
+elseif strcmp(conditions, 'previous')
+    whichstat = 'statHemi';%'statHemi'; %statCvsN statICvsN
+end
 
 frequency = [10 20 40:4:100];
 cnt = 0;
 for k = frequency
   fprintf('computing T-statistic for frequency %d Hz\n', k);
-  d = dir(fullfile(datadir,sprintf('*3d4mm*pre*_%d.mat',k)));
-  for m = 1:numel(d)
-    load(fullfile(d(m).folder,d(m).name),'statResp');
-    tmp(m) = statResp;
+  if strcmp(conditions, 'current_previous')
+      d = dir(fullfile(datadir,sprintf('*3d4mm*pre_curprev_%d.mat',k)));
+  elseif strcmp(conditions, 'previous')
+      d = dir(fullfile(datadir,sprintf('*3d4mm*pre_%d.mat',k)));
   end
+  for m = 1:numel(d)
+    dum = load(fullfile(d(m).folder,d(m).name),whichstat);
+    tmp(m) = dum.(whichstat);
+  end
+  clear dum
   dat = cat(2,tmp.stat);
   n   = size(dat,2);
   for m = 1:n
@@ -43,8 +54,7 @@ source_int.gamma1(~isfinite(source_int.gamma1))=0;
 source_int.gamma2(~isfinite(source_int.gamma2))=0;
 
 cmap = flipud(brewermap(64,'RdBu'));
-
-cfgp.funparameter  = 'gamma1';
+cfgp=[];
 cfgp.maskparameter = 'mask';
 cfgp.funcolormap = cmap;
 cfgp.maskstyle  = 'colormix';
@@ -52,5 +62,23 @@ cfgp.method     = 'slice';
 cfgp.nslices    =  30;
 cfgp.slicerange =  [40 150];
 cfgp.opacitylim = [2 4];
-cfgp.funcolorlim = [-4 4];
 
+% cfgp.method = 'surface';
+
+cfgp.funcolorlim = [-3 3];
+cfgp.funparameter = 'alpha';
+ft_sourceplot(cfgp, source_int);
+title('alpha'); pause(0.01); 
+
+cfgp.funcolorlim = [-3 3];
+cfgp.funparameter = 'beta';
+ft_sourceplot(cfgp, source_int);
+title('beta'); pause(0.01); 
+
+cfgp.funcolorlim = [-2 2];
+cfgp.funparameter  = 'gamma1';
+ft_sourceplot(cfgp, source_int);
+title('gamma1'); pause(0.01); 
+cfgp.funparameter  = 'gamma2';
+ft_sourceplot(cfgp, source_int);
+title('gamma2'); pause(0.01); 
