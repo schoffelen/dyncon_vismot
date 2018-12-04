e9fed74a8029975e63f9b0040aafa8fb0f67e06a
Mats van Es <m.vanes@donders.ru.nl>
2018-12-04 12:16:43 +0100

    no changes

---
+++ matlab/vismot_script_Tstat_pre.m
@@ -0,0 +1,77 @@
+datadir = '/project/3011085.03/analysis/source';
+whichstat = 'statResp';%'statHemi'; %statCvsN statICvsN
+
+
+frequency = [4:2:30 40:4:100];
+cnt = 0;
+for k = frequency
+    fprintf('computing T-statistic for frequency %d Hz\n', k);
+    
+    d = dir(fullfile(datadir,sprintf('*3d4mm*pre_%03d.mat',k)));
+    for m = 1:numel(d)
+        dum = load(fullfile(d(m).folder,d(m).name),whichstat);
+        tmp(m) = dum.(whichstat);
+    end
+    clear dum
+    dat = cat(2,tmp.stat);
+    n   = size(dat,2);
+    for m = 1:n
+        dat(:,m+n) = nanmean(dat(:,m));
+    end
+    
+    design   = [ones(1,n) ones(1,n)*2;1:n 1:n];
+    cfg.ivar = 1;
+    cfg.uvar = 2;
+    tmp      = ft_statfun_depsamplesT(cfg, dat, design);
+    cnt      = cnt+1;
+    T(:,cnt) = tmp.stat;
+    clear tmp;
+    
+end
+
+load standard_sourcemodel3d4mm;
+mri = ft_read_mri('single_subj_T1_1mm.nii');
+source = sourcemodel;
+source.gamma1 = nanmean(T(:,nearest(frequency,40):nearest(frequency,60)),2);
+source.gamma2 = nanmean(T(:,nearest(frequency,70):nearest(frequency,90)),2);
+source.alpha  = T(:,1);
+source.beta   = T(:,2);
+
+cfgi = [];
+cfgi.parameter = {'alpha' 'beta' 'gamma1' 'gamma2'};
+source_int = ft_sourceinterpolate(cfgi, source, mri);
+
+source_int.alpha(~isfinite(source_int.alpha))=0;
+source_int.beta(~isfinite(source_int.beta))=0;
+source_int.gamma1(~isfinite(source_int.gamma1))=0;
+source_int.gamma2(~isfinite(source_int.gamma2))=0;
+
+cmap = flipud(brewermap(64,'RdBu'));
+cfgp=[];
+cfgp.maskparameter = 'mask';
+cfgp.funcolormap = cmap;
+cfgp.maskstyle  = 'colormix';
+cfgp.method     = 'slice';
+cfgp.nslices    =  30;
+cfgp.slicerange =  [40 150];
+cfgp.opacitylim = [2 4];
+
+% cfgp.method = 'surface';
+
+cfgp.funcolorlim = [-3 3];
+cfgp.funparameter = 'alpha';
+ft_sourceplot(cfgp, source_int);
+title('alpha'); pause(0.01);
+
+cfgp.funcolorlim = [-3 3];
+cfgp.funparameter = 'beta';
+ft_sourceplot(cfgp, source_int);
+title('beta'); pause(0.01);
+
+cfgp.funcolorlim = [-2 2];
+cfgp.funparameter  = 'gamma1';
+ft_sourceplot(cfgp, source_int);
+title('gamma1'); pause(0.01);
+cfgp.funparameter  = 'gamma2';
+ft_sourceplot(cfgp, source_int);
+title('gamma2'); pause(0.01);
