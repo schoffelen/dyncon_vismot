df7f25029ee63f5fc58058a1389b83adfeae468b
Mats van Es <m.vanes@donders.ru.nl>
2018-12-18 17:03:38 +0100

    generalized for multiple trials

--- matlab/hemiflip.m
+++ matlab/hemiflip.m
@@ -5,24 +5,29 @@ function dataout = hemiflip(datain, parameter)
 % parameter(s) to be flipped.
 % NOTE: only works when parameter is 2D array.
 
+ntrials = size(datain.(parameter),2);
+
 dataout = datain;
 if iscell(parameter)
     for k=1:numel(parameter)
         p = parameter{k};
         if isfield(datain, 'tri')
             n = size(datain.(p),1)./2;
-            
-            dataout.(p) = datain.(p)([n+(1:n) 1:n],1);
+            dataout.(p) = datain.(p)([n+(1:n) 1:n],:);
         elseif isfield(datain, 'dim')
-            dataout.(p) = reshape(flip(reshape(datain.(p), datain.dim),1),[],1);
+            for m=1:ntrials
+                dataout.(p)(:,m) = reshape(flip(reshape(datain.(p)(:,m), datain.dim),1),[],1);
+            end
         end
     end
 else
     if isfield(datain, 'tri')
         n = size(datain.(parameter),1)./2;
-        dataout.(parameter) = datain.(parameter)([n+(1:n) 1:n],1);
+        dataout.(parameter) = datain.(parameter)([n+(1:n) 1:n],:);
     elseif isfield(datain, 'dim')
-        dataout.(parameter) = reshape(flip(reshape(datain.(parameter), datain.dim),1),[],1);
+        for m=1:ntrials
+            dataout.(parameter)(:,m) = reshape(flip(reshape(datain.(parameter)(:,m), datain.dim),1),[],1);
+        end
     end
 end
 
