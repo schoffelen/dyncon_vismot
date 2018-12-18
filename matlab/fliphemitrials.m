d1df9ca936cd81bf744872c2726d5a9b72562961
Mats van Es <m.vanes@donders.ru.nl>
2018-12-18 17:02:54 +0100

    first commit

---
+++ matlab/fliphemitrials.m
@@ -0,0 +1,25 @@
+function dataout = fliphemitrials(datain, parameter, trlinfo_column, flip_from, flip_to)
+
+cfg=[];
+cfg.trials = ismember(datain.trialinfo(:,trlinfo_column), flip_from);
+fliptrials = ft_selectdata(cfg, datain);
+cfg.trials = ~ismember(datain.trialinfo(:,trlinfo_column), flip_from);
+nofliptrials = ft_selectdata(cfg, datain);
+
+fliptrials = hemiflip(fliptrials, parameter);
+
+% some hacky way to append data (doesnt work with ft_appendsource because
+% of size of trialinfo)
+dataout = nofliptrials;
+dataout.(parameter) = [dataout.(parameter), fliptrials.(parameter)];
+dataout.trialinfo = [dataout.trialinfo; fliptrials.trialinfo];
+
+% change trialinfo
+before = dataout.trialinfo(:,trlinfo_column);
+after = before;
+for k=1:numel(flip_from)
+    after(before==flip_from(k)) = flip_to(k);
+end
+
+dataout.trialinfo = after;
+end
