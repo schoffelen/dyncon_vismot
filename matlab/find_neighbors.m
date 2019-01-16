120e7fb7c0de389641c810c77e23bd9bfe3ab8e7
Mats van Es <m.vanes@donders.ru.nl>
2019-01-16 14:37:35 +0100

    bugfix: resolution was wrongly determined

--- matlab/find_neighbors.m
+++ matlab/find_neighbors.m
@@ -2,10 +2,7 @@ function [output, seed] = find_neighbors(seed, sourcemodel)
 %assume seed and sourcemodel in same units
 % determine resolution
 xpos = sourcemodel.pos(:,1);
-ref = xpos(1);
-nonzero_pos = abs(ref-xpos)>0;
-res = min(abs(xpos(nonzero_pos))); % in same units as sourcemodel
-
+resolution = mode(diff(unique(xpos))); % in same units as sourcemodel
 
 if size(seed,2)==1
     % assume seed are indices, covert to x,y,z pos
@@ -15,10 +12,10 @@ end
 output=[];
 for k=1:size(seed,1)
     output(end+1,:) = seed(k,:);
-    output(end+1,:) = seed(k,:) + [-1 0 0]*res;
-    output(end+1,:) = seed(k,:) + [1 0 0]*res;
-    output(end+1,:) = seed(k,:) + [0 -1 0]*res;
-    output(end+1,:) = seed(k,:) + [0 1 0]*res;
-    output(end+1,:) = seed(k,:) + [0 0 -1]*res;
-    output(end+1,:) = seed(k,:) + [0 0 1]*res;
+    output(end+1,:) = seed(k,:) + [-1 0 0]*resolution;
+    output(end+1,:) = seed(k,:) + [1 0 0]*resolution;
+    output(end+1,:) = seed(k,:) + [0 -1 0]*resolution;
+    output(end+1,:) = seed(k,:) + [0 1 0]*resolution;
+    output(end+1,:) = seed(k,:) + [0 0 -1]*resolution;
+    output(end+1,:) = seed(k,:) + [0 0 1]*resolution;
 end
