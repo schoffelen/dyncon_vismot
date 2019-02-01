1fb4bd25fc0aeeb23c34bb717719ed41da881434
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2019-02-01 15:35:54 +0100

    ENH - update to symmetrize inside

--- matlab/vismot_bf_coh_stratified_script.m
+++ matlab/vismot_bf_coh_stratified_script.m
@@ -19,10 +19,30 @@ end
 if ~exist('lambda', 'var')
   lambda = '20%';
 end
+if ~exist('N', 'var')
+  N = 75;
+end
 subject = vismot_subjinfo(subjectname);
 load(fullfile(subject.pathname,'grid',sprintf('%s_sourcemodel3d10mm',subject.name)),'sourcemodel');
 
-N = 75;
+% prune the inside, so that only the superficial dipoles are used
+dum = zeros(sourcemodel.dim);
+dum(sourcemodel.inside) = 1;
+dum = dum>0;
+se  = cat(3,[0 0 0;0 1 0;0 0 0],[0 1 0;1 1 1;0 1 0],[0 0 0;0 1 0;0 0 0])>0;
+dum2 = dum;
+dum2 = imerode(imerode(imerode(dum,se),se),se);
+sourcemodel.inside = (double(dum(:))-double(dum2(:)))>0;
+
+% and then left/right symmetrize in anticipation of collapsing across
+% left/right responses
+dum = zeros(sourcemodel.dim);
+dum(sourcemodel.inside) = 1;
+dum2 = flip(dum,1);
+dum = double(dum)+double(dum2);
+sourcemodel.inside(:) = dum==2;
+
+
 stratflag = 2;
 [coh13,coh42,zx13,zx42,looptime] = vismot_bf_coh_stratified(subject,'sourcemodel',sourcemodel,'nrand',nrand,'stratflag', stratflag, 'N', N, 'lambda', lambda);
 
@@ -36,4 +56,4 @@ coh13 = rmfield(coh13, {'coh_1', 'coh_2'});
 coh42 = rmfield(coh42, {'coh_1', 'coh_2'});
 
 filename = fullfile(subject.pathname,'source', [subject.name, '_coh6d10mm_',sprintf('%03d', frequency)] );
-save(filename, 'zx13', 'zx42', 'coh13', 'coh42');
+save(filename, 'zx13', 'zx42', 'coh13', 'coh42', 'sourcemodel', 'N', 'nrand');
