aff9ddaf1657692f5cf7faece1d46fb5693502a4
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2019-04-18 16:39:06 +0200

    ENH - some updates

---
+++ matlab/vismot_parcellation_stuff.m
@@ -0,0 +1,276 @@
+label = {
+  'L_6_B05_01'  'L06.4'
+  'L_6_B05_02'  'L06.6'
+  'L_6_B05_03'  'L06.5'
+  'L_6_B05_04'  'L06.2'
+  'L_6_B05_05'  'L06.5'
+  'L_6_B05_06'  'L06.4'
+  'L_6_B05_07'  'L06.3'
+  'L_6_B05_08'  'L06.1' 
+  'L_6_B05_09'  'L06.2'
+  'L_6_B05_10'  'L06.3'
+  'L_6_B05_11'  'L06.1'
+  'L_4_B05_01'  'L04.3'
+  'L_4_B05_02'  'L04.6'
+  'L_4_B05_03'  'L04.5'
+  'L_4_B05_04'  'L04.2'
+  'L_4_B05_05'  'L04.6'
+  'L_4_B05_06'  'L04.3'
+  'L_4_B05_07'  'L04.4'
+  'L_4_B05_08'  'L04.5'
+  'L_4_B05_09'  'L04.1'
+  'L_4_B05_10'  'L04.1'
+  'L_4_B05_11'  'L04.2'
+  'L_4_B05_12'  'L04.4'
+  'L_3_B05_01'  'L123.4'
+  'L_3_B05_02'  'L123.2'
+  'L_3_B05_03'  'L123.1'
+  'L_1_B05_01'  'L123.4'
+  'L_1_B05_02'  'L123.3'
+  'L_1_B05_03'  'L123.5'
+  'L_1_B05_04'  'L123.1'
+  'L_1_B05_05'  'L123.2'
+  'L_5_B05_01'  'L05.2'
+  'L_5_B05_02'  'L05.2'
+  'L_5_B05_03'  'L05.1'
+  'L_7_B05_01'  'L07.3'
+  'L_7_B05_02'  'L07.2'
+  'L_7_B05_03'  'L07.1'
+  'L_7_B05_04'  'L07.5'
+  'L_7_B05_05'  'L07.2'
+  'L_7_B05_06'  'L07.7'
+  'L_7_B05_07'  'L07.1'
+  'L_7_B05_08'  'L07.4'
+  'L_7_B05_09'  'L07.3'
+  'L_7_B05_10'  'L07.5'
+  'L_7_B05_11'  'L07.6'
+  'L_7_B05_12'  'L07.4'
+  'L_7_B05_13'  'L07.6'
+  'L_2_B05_01'  'L123.6'
+  'L_2_B05_02'  'L123.7'
+  'L_2_B05_03'  'L123.7'
+  'L_2_B05_04'  'L123.7'
+  'L_2_B05_05'  'L123.5'
+  'L_2_B05_06'  'L123.1'
+  'L_2_B05_07'  'L123.8'
+  'L_2_B05_08'  'L123.3'
+  'L_19_B05_01' 'L19.4'
+  'L_19_B05_02' 'L19.5'
+  'L_19_B05_03' 'L19.7'
+  'L_19_B05_04' 'L19.4'
+  'L_19_B05_05' 'L19.5'
+  'L_19_B05_06' 'L19.1'
+  'L_19_B05_07' 'L19.6'
+  'L_19_B05_08' 'L19.2'
+  'L_19_B05_09' 'L19.3'
+  'L_19_B05_10' 'L19.6'
+  'L_19_B05_11' 'L19.7'
+  'L_19_B05_12' 'L19.3'
+  'L_19_B05_13' 'L19.1'
+  'L_19_B05_14' 'L19.2'
+  'L_17_B05_01' 'L17.1'
+  'L_17_B05_02' 'L17.2'
+  'L_17_B05_03' 'L17.3'
+  'L_17_B05_04' 'L17.4'
+  'L_18_B05_01' 'L18.1'
+  'L_18_B05_02' 'L18.2'
+  'L_18_B05_03' 'L18.3'
+  'L_18_B05_04' 'L18.4'
+  'L_18_B05_05' 'L18.5'
+  'L_18_B05_06' 'L18.6'
+  'L_18_B05_07' 'L18.7'
+  'R_6_B05_01'  'R06.4'
+  'R_6_B05_02'  'R06.6'
+  'R_6_B05_03'  'R06.5'
+  'R_6_B05_04'  'R06.2'
+  'R_6_B05_05'  'R06.5'
+  'R_6_B05_06'  'R06.4'
+  'R_6_B05_07'  'R06.3'
+  'R_6_B05_08'  'R06.1' 
+  'R_6_B05_09'  'R06.2'
+  'R_6_B05_10'  'R06.3'
+  'R_6_B05_11'  'R06.1'
+  'R_4_B05_01'  'R04.3'
+  'R_4_B05_02'  'R04.6'
+  'R_4_B05_03'  'R04.5'
+  'R_4_B05_04'  'R04.2'
+  'R_4_B05_05'  'R04.6'
+  'R_4_B05_06'  'R04.3'
+  'R_4_B05_07'  'R04.4'
+  'R_4_B05_08'  'R04.5'
+  'R_4_B05_09'  'R04.1'
+  'R_4_B05_10'  'R04.1'
+  'R_4_B05_11'  'R04.2'
+  'R_4_B05_12'  'R04.4'
+  'R_3_B05_01'  'R123.4'
+  'R_3_B05_02'  'R123.2'
+  'R_3_B05_03'  'R123.1'
+  'R_1_B05_01'  'R123.4'
+  'R_1_B05_02'  'R123.3'
+  'R_1_B05_03'  'R123.5'
+  'R_1_B05_04'  'R123.1'
+  'R_1_B05_05'  'R123.2'
+  'R_5_B05_01'  'R05.2'
+  'R_5_B05_02'  'R05.2'
+  'R_5_B05_03'  'R05.1'
+  'R_7_B05_01'  'R07.3'
+  'R_7_B05_02'  'R07.2'
+  'R_7_B05_03'  'R07.1'
+  'R_7_B05_04'  'R07.5'
+  'R_7_B05_05'  'R07.2'
+  'R_7_B05_06'  'R07.7'
+  'R_7_B05_07'  'R07.1'
+  'R_7_B05_08'  'R07.4'
+  'R_7_B05_09'  'R07.3'
+  'R_7_B05_10'  'R07.5'
+  'R_7_B05_11'  'R07.6'
+  'R_7_B05_12'  'R07.4'
+  'R_7_B05_13'  'R07.6'
+  'R_2_B05_01'  'R123.6'
+  'R_2_B05_02'  'R123.7'
+  'R_2_B05_03'  'R123.7'
+  'R_2_B05_04'  'R123.7'
+  'R_2_B05_05'  'R123.5'
+  'R_2_B05_06'  'R123.1'
+  'R_2_B05_07'  'R123.8'
+  'R_2_B05_08'  'R123.3'
+  'R_19_B05_01' 'R19.4'
+  'R_19_B05_02' 'R19.5'
+  'R_19_B05_03' 'R19.7'
+  'R_19_B05_04' 'R19.4'
+  'R_19_B05_05' 'R19.5'
+  'R_19_B05_06' 'R19.1'
+  'R_19_B05_07' 'R19.6'
+  'R_19_B05_08' 'R19.2'
+  'R_19_B05_09' 'R19.3'
+  'R_19_B05_10' 'R19.6'
+  'R_19_B05_11' 'R19.7'
+  'R_19_B05_12' 'R19.3'
+  'R_19_B05_13' 'R19.1'
+  'R_19_B05_14' 'R19.2'
+  'R_17_B05_01' 'R17.1'
+  'R_17_B05_02' 'R17.2'
+  'R_17_B05_03' 'R17.3'
+  'R_17_B05_04' 'R17.4'
+  'R_18_B05_01' 'R18.1'
+  'R_18_B05_02' 'R18.2'
+  'R_18_B05_03' 'R18.3'
+  'R_18_B05_04' 'R18.4'
+  'R_18_B05_05' 'R18.5'
+  'R_18_B05_06' 'R18.6'
+  'R_18_B05_07' 'R18.7'};
+
+ft_hastoolbox('brewermap', 1);
+
+atlasdir = '/project/3011085.03/analysis/mri/Conte69_32k';
+load(fullfile(atlasdir,'atlas_subparc374_32k'));
+meshfile = fullfile(atlasdir, 'Conte69.L.inflated.32k_fs_LR.surf.gii');
+inflated = ft_read_headshape(meshfile);
+mesh     = ft_read_headshape(fullfile(atlasdir,'colin.cerebral.L.flat.32k_fs_LR.surf.gii'));
+
+cmap6 = brewermap(11,'blues'); % area 6
+cmap4 = brewermap(12,'reds');  % area 4
+cmap3 = brewermap(3, 'greens');
+cmap1 = brewermap(5, 'purples');
+cmap5 = brewermap(3, 'oranges');
+cmap7 = brewermap(13,'reds');
+cmap2 = brewermap(8, 'blues');
+cmap19= brewermap(14,'purples');
+cmap17= brewermap(4, 'greens');
+cmap18= brewermap(7, 'oranges');
+
+cmap = cat(1,cmap6,cmap4,cmap3,cmap1,cmap5,cmap7,cmap2,cmap19,cmap17,cmap18);
+cmap = cat(1,cmap,cmap);
+
+cmap(end+1,:) = [.7 .7 .7];
+
+[a,b] = match_str(atlas.parcellationlabel, label);
+X     = atlas.parcellation;
+X(~ismember(X,a)) = nan;
+for k = 1:numel(a)
+  X(X==a(k))=k;
+end
+X(~isfinite(X)) = numel(a)+1;
+X = X(1:32492);
+
+figure;ft_plot_mesh(mesh, 'vertexcolor', cmap(X(1:32492),:));
+
+% compute centroids of parcels to plot labels;
+for k = 1:numel(label(:,1))
+  pos(k,:) = median(mesh.pos(X==k,:),1);
+end
+
+
+labelshort = strrep(label(:,1),'_B05','');
+labelshort = strrep(labelshort,'L_','L');
+labelshort = strrep(labelshort,'R_','R');
+labelshort = strrep(labelshort,'_','.');
+
+figure;ft_plot_mesh(mesh, 'vertexcolor', cmap(X,:));
+hold on;
+ft_plot_mesh(pos);
+ft_plot_text(pos(:,1), pos(:,2)+5, labelshort(1:80));
+
+% group the parcel into somewhat larger chunks to keep the circleplots
+% manageable
+labelchunked = label(:,2);
+
+ulabel = unique(labelchunked);
+Y = zeros(size(X));
+P = zeros(numel(ulabel), size(label,1));
+for k = 1:numel(ulabel)
+  sel = match_str(labelchunked,ulabel(k));
+  Y(ismember(X,sel)) = k;
+  P(k,sel) = 1./numel(sel);
+end
+Y(Y==0) = size(cmap,1);
+figure;ft_plot_mesh(mesh, 'vertexcolor', cmap(Y,:));
+
+reorder = [find(contains(ulabel,'R06'));
+  find(contains(ulabel,'R04'));
+  find(contains(ulabel,'R123'));
+  find(contains(ulabel,'R05'));
+  find(contains(ulabel,'R07'));
+  find(contains(ulabel,'R19'));
+  find(contains(ulabel,'R18'));
+  find(contains(ulabel,'R17'));
+  flipud(find(contains(ulabel,'L17')));
+  flipud(find(contains(ulabel,'L18')));
+  flipud(find(contains(ulabel,'L19')));
+  flipud(find(contains(ulabel,'L07')));
+  flipud(find(contains(ulabel,'L05')));
+  flipud(find(contains(ulabel,'L123')));
+  flipud(find(contains(ulabel,'L04')));
+  flipud(find(contains(ulabel,'L06')))];
+
+P = P(reorder,:);
+ulabel = ulabel(reorder);
+
+dat.label = ulabel;
+dat.pow   = zeros(numel(ulabel),1);
+dat.freq  = 0;
+dat.dimord = 'chan_freq';
+
+cfgl = [];
+cfgl.layout = 'circular';
+layout = ft_prepare_layout(cfgl, dat);
+
+% rotate the positions a bit
+angle = -acos(layout.pos(2,:)*layout.pos(1,:)')./2;
+R     = [cos(angle) -sin(angle);sin(angle) cos(angle)];
+layout.pos = (R*layout.pos')';
+
+laynew     = layout;
+laynew.pos = laynew.pos.*1.05;
+
+% set some parameters for the layout plotting
+rot = 360*atan(laynew.pos(:,2)./laynew.pos(:,1))/(2*pi);
+n   = numel(laynew.label)/2;
+alh = [repmat({'left'},[n 1]);repmat({'right'},[n 1])];
+
+% re-set the options for the layout plotting
+plotlayoptions = {'interpreter','none','point','no','box','no','labelrotate',rot,'labelalignh',alh,'labelsize',16};
+
+figure;
+ft_plot_lay(laynew, plotlayoptions{:});
+axis([-1.2 1.2 -1.2 1.2]);
