92b21e043fc98fc9e11186b66ccbf48de88df249
Mats van Es <m.vanes@donders.ru.nl>
2019-05-03 15:40:02 +0200

    remove unique(trialnumber), not necessary anymore.

--- matlab/vismot_mim_script_singletrial.m
+++ matlab/vismot_mim_script_singletrial.m
@@ -1,16 +1,15 @@
 
 load list
-subjectname = list{1};
+subjectname = list{2};
 subject = vismot_subjinfo(subjectname);
 
 load(sprintf('/project/3011085.03/analysis/trl/%s_trialnumber_%s', subject.name, 'previous'));
-trialnumber = unique(trialnumber);
 ntrials = numel(trialnumber);
 
 for k=1:ntrials
   leaveouttrial = trialnumber(k);
   
-qsubfeval(@vismot_execute_pipeline, 'vismot_mim_script', subject.name, {'prewhiten',true},{'split',false}, {'conditions', [1:5]}, {'dobaseline', 0}, {'doL1out', 1}, {'leaveouttrial', leaveouttrial}, 'memreq', 10*1024^3, 'timreq', 1800, 'batchid', sprintf('mim_%d', leaveouttrial));
+qsubfeval(@vismot_execute_pipeline, 'vismot_mim_script', subject.name, {'prewhiten',true},{'split',false}, {'conditions', [1:5]}, {'dobaseline', 0}, {'doL1out', 1}, {'leaveouttrial', leaveouttrial}, 'memreq', 8*1024^3, 'timreq', 900, 'batchid', sprintf('mim_%s_%d', subjectname, leaveouttrial));
 
 end
-qsubfeval(@vismot_execute_pipeline, 'vismot_mim_script', subject.name, {'prewhiten',true},{'split',false}, {'conditions', [1:5]}, {'dobaseline', 0}, {'doL1out', 0}, {'leaveouttrial', []}, 'memreq', 10*1024^3, 'timreq', 1800, 'batchid', 'mim_all');
+qsubfeval(@vismot_execute_pipeline, 'vismot_mim_script', subject.name, {'prewhiten',true},{'split',false}, {'conditions', [1:5]}, {'dobaseline', 0}, {'doL1out', 0}, 'memreq', 10*1024^3, 'timreq', 800, 'batchid', sprintf('mim_%s_all', subjectname));
