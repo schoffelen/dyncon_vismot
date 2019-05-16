78bd73c6ab375ba672464d61bf3bf494ec140427
Mats van Es <m.vanes@donders.ru.nl>
2019-05-16 15:37:39 +0200

    add option for resending jobs if the didn't execute successfully.

--- matlab/vismot_mim_script_singletrial.m
+++ matlab/vismot_mim_script_singletrial.m
@@ -1,15 +1,31 @@
-
+for l=1
 load list
-subjectname = list{2};
+subjectname = list{l};
 subject = vismot_subjinfo(subjectname);
 
 load(sprintf('/project/3011085.03/analysis/trl/%s_trialnumber_%s', subject.name, 'previous'));
-ntrials = numel(trialnumber);
+ntrials = numel(trialinfo(:,2));
+% in case not all jobs completed successfully
+%{
+filename = ['/project_ext/3010029/reproducescript/analysis/mim/singletrial/', subjectname, '/', '*.mat'];
+d = dir(filename);
+for k = 1:numel(d)
+k;
+% m{k} = load([d(k).folder,'/', d(k).name]);
+donetrials(k) = str2num(char(extractBetween(d(k).name,"all_", ".mat")));
+end
+numel(trialnumber)
+trialnumber = trialnumber(~ismember(trialnumber, donetrials));
+numel(trialnumber)
 
-for k=1:ntrials
+%}
+for k=1:numel(trialnumber)
   leaveouttrial = trialnumber(k);
   
-qsubfeval(@vismot_execute_pipeline, 'vismot_mim_script', subject.name, {'prewhiten',true},{'split',false}, {'conditions', [1:5]}, {'dobaseline', 0}, {'doL1out', 1}, {'leaveouttrial', leaveouttrial}, 'memreq', 8*1024^3, 'timreq', 900, 'batchid', sprintf('mim_%s_%d', subjectname, leaveouttrial));
+qsubfeval(@vismot_execute_pipeline, 'vismot_mim_script', subject.name, {'prewhiten',true},{'split',false}, {'conditions', [1:4]}, {'dobaseline', 0}, {'doL1out', 1}, {'leaveouttrial', leaveouttrial}, 'memreq', 8*1024^3, 'timreq', 3600, 'batchid', sprintf('mim_%s_%d', subjectname, leaveouttrial));
 
 end
-qsubfeval(@vismot_execute_pipeline, 'vismot_mim_script', subject.name, {'prewhiten',true},{'split',false}, {'conditions', [1:5]}, {'dobaseline', 0}, {'doL1out', 0}, 'memreq', 10*1024^3, 'timreq', 800, 'batchid', sprintf('mim_%s_all', subjectname));
+qsubfeval(@vismot_execute_pipeline, 'vismot_mim_script', subject.name, {'prewhiten',true},{'split',false}, {'conditions', [1:4]}, {'dobaseline', 0}, {'doL1out', 0}, 'memreq', 10*1024^3, 'timreq', 3600, 'batchid', sprintf('mim_%s_all', subjectname));
+
+clear donetrials trialnumber
+end
