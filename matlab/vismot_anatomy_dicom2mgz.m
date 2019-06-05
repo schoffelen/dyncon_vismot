0f2a3a0fc988fe7262d3946d357abf44e40a1f46
Mats van Es <m.vanes@donders.ru.nl>
2019-06-05 17:07:48 +0200

    make consistent with current pipeline (in terms of loading/saving data, directories etc).

--- matlab/vismot_anatomy_dicom2mgz.m
+++ matlab/vismot_anatomy_dicom2mgz.m
@@ -1,26 +1,31 @@
-function vismot_anatomy_dicom2mgz
+function vismot_anatomy_dicom2mgz(subject)
 
-subjinfo;
+if nargin<1
+  subject = vismot_subjinfo;
+elseif ischar(subject)
+  subject = vismot_subjinfo(subject);
+end
+  
 
-for k = 5:numel(SUBJ)
-  if exist(fullfile(SUBJ(k).pathname,'mri',[SUBJ(k).name,'.mgz']),'file')
+for k = 1:numel(subject)
+  if exist(fullfile(subject(k).pathname,'mri',[subject(k).name,'.mgz']),'file')
     continue;
   end
   
-  if ~isempty(SUBJ(k).mriname)
-    name = SUBJ(k).mriname;
+  if ~isempty(subject(k).mriname)
+    name = subject(k).mriname;
   else
-    name = SUBJ(k).name;
+    name = subject(k).name;
   end
-  fprintf('processing subject %s\n',SUBJ(k).name);
+  fprintf('processing subject %s\n',subject(k).name);
 
-  cd(SUBJ(k).rawpath);
+  cd(subject(k).rawpath);
   cd([name,'_RAW']);
   d   = dir([upper(name),'*']);
   mri = ft_read_mri(d(end).name);
   
   cfg = [];
-  cfg.filename = fullfile(SUBJ(k).pathname,'mri',[SUBJ(k).name,'.mgz']);
+  cfg.filename = fullfile(subject(k).pathname,'mri',[subject(k).name,'.mgz']);
   cfg.filetype = 'mgz';
   cfg.parameter = 'anatomy';
   ft_volumewrite(cfg, mri);
