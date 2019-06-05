8cf3cb1d15a4cb1e93b7483ea1a0599b266c3e61
Mats van Es <m.vanes@donders.ru.nl>
2019-06-05 17:38:31 +0200

    first commit.

--- matlab/vismot_bf_svm_postprocess.m
+++ matlab/vismot_bf_svm_postprocess.m
@@ -6,97 +6,103 @@ nfreq = 4;
 nfolds = 5;
 load /project/3011085.03/scripts/fieldtrip/template/sourcemodel/standard_sourcemodel3d4mm.mat
 ninsidepos = sum(sourcemodel.inside);
-
+freqs = [10 20 40 60];
 % prepare matrices
-randprimal = zeros(numrandomization,6,ninsidepos,nfreq);
-primal = zeros(n,6,ninsidepos,nfreq);
+% primal = zeros(n,6,ninsidepos,nfreq);
+primal = zeros(n,ninsidepos,nfreq);
 
 randacc = zeros(n,numrandomization);
+for q=[1 2];
 for k=1:n
     k
     subjectname = list{k};
     subject = vismot_subjinfo(subjectname);
-    filename = fullfile(subject.pathname,'pow', [subject.name, '_source3d4mm_pre_svm.mat']);
-    M = load(filename, 'stat');
-    
+    filename = fullfile(subject.pathname,'pow', [subject.name, '_source3d4mm_pre_svm1342.mat']);
+    M = load(filename);
+    if q==1
+      M.stat=M.stat13;
+    elseif q==2
+      M.stat = M.stat42;
+    end
     for l=1:nfolds
-        for m=1:6
-            tmpprimal(l,m,:) = M.stat.model{l}.model{m}.primal;
-        end
+%         for m=1%:6
+%             tmpprimal(l,m,:) = M.stat.model{l}.model{m}.primalinv;
+            tmpprimal(l,:) = M.stat.model{l}.primal;
+%         end
     end
-    primal(k,:,:,:) = reshape(mean(tmpprimal,1), [6, ninsidepos, nfreq]);
-    
-    tmp2 = zeros(numrandomization,6, ninsidepos,nfreq);
-    for l=1:numrandomization
-        %       filename = fullfile(subject.pathname,'pow','rand', [subject.name, sprintf('_source3d4mm_pre_naive_rand%d.mat', l)]);
-        filename = sprintf('/project_ext/3010029/reproducescript/vismsot/analysis/pow/rand/%s_source3d4mm_pre_svm_rand%d.mat', subject.name, l);
-        randtmp = load(filename);
-        
-        randacc(k,l) = randtmp.stat.statistic.accuracy;
-        tmp1 = zeros(nfolds, 6, ninsidepos, nfreq);
-        
-        for m=1:nfolds % number of folds
-            for jj=1:6
-            tmp1(m,jj,:,:) = reshape(randtmp.stat.model{m}.model{jj}.primal, ninsidepos, nfreq);
-            end
-        end
-        tmp2(l,:,:,:) = squeeze(nanmean(tmp1,1));
-    end
-    randprimal(k, :, :, :) = nanmean(tmp2,1);
-
-    acc(k,1) = M.stat.statistic.accuracy;
-%     model{k} = M.model;
-    p(k) = (sum(randacc(k,:)>=acc(k))+1)/(numrandomization+1); % See Ojala & Garriga 2010 (JMLR)
-    
-    diff{k} = sourcemodel;
-    diff{k}.primal = zeros(6, numel(sourcemodel.inside), 4);
-    diff{k}.primal(:,diff{k}.inside,:)=squeeze(primal(k,:,:,:))./squeeze(mean(randprimal,1));
+%     primal(k,:,:,:) = reshape(mean(tmpprimal,1), [6, ninsidepos, nfreq]);
+if q==1
+        primal13(k,:,:) = reshape(mean(tmpprimal,1), [ninsidepos, nfreq]);
+elseif q==2
+  primal42(k,:,:) = reshape(mean(tmpprimal,1), [ninsidepos, nfreq]);
 end
 
+    for l=1:numrandomization
+        %       filename = fullfile(subject.pathname,'pow','rand', [subject.name, sprintf('_source3d4mm_pre_naive_rand%d.mat', l)]);
+        filename = sprintf('/project_ext/3010029/reproducescript/vismsot/analysis/pow/rand/%s_source3d4mm_pre_svm1342_rand%d.mat', subject.name, l);
+        randtmp = load(filename);
+        if q==1
+          randtmp.statistic = randtmp.statistic13;
+          randacc13(k,l) = randtmp.statistic.accuracy;
+        elseif q==2
+          randtmp.statistic = randtmp.statistic42;
+          randacc42(k,l) = randtmp.statistic.accuracy;
+        end
+    end
+if q==1
+    acc13(k,1) = M.stat.statistic.accuracy;
+elseif q==2;
+  acc42(k,1) = M.stat.statistic.accuracy;
+end
+%     model{k} = M.model;
+if q==1
+      p(k) = (sum(randacc13(k,:)>=acc13(k))+1)/(numrandomization+1); % See Ojala & Garriga 2010 (JMLR)
+elseif q==2
+  p(k) = (sum(randacc42(k,:)>=acc42(k))+1)/(numrandomization+1); % See Ojala & Garriga 2010 (JMLR)
+end
+end
+end
 
 figure;
 for k=1:n
     subplot(4,5,k);hold on
-    histogram(randacc(k,:)); vline(acc(k));
+    histogram(randacc42(k,:)); vline(acc42(k));
     title(sprintf('p=%d', p(k)));
 end
 
+for k=1:n
+weights{k} = sourcemodel;
+weights{k}.stat = zeros(numel(sourcemodel.inside), nfreq);
+% weights{k}.stat(sourcemodel.inside,:) = squeeze(squeeze(nanmean(primal(k,5,:,:),2)));
+weights{k}.stat(sourcemodel.inside,:) = squeeze(primal(k,:,:));
+weights{k}.dimord = 'pos_freq';
+weights{k}.freq = freqs;
 
-idx_sign = find(p<0.05);
-nsign = numel(idx_sign);
-diff2 = diff(idx_sign);
-
-for k=1:nsign
-    diff2{k}.primal = zeros(6,numel(sourcemodel.inside), 4);
-    diff2{k}.primal(:,sourcemodel.inside,:) = squeeze(primal(k,:,:,:));
-    diff2{k}.pos = sourcemodel.pos;
-    nul{k} = diff2{k};
-    nul{k}.primal = 0*nul{k}.primal;
+nul{k} = weights{k};
+nul{k}.stat = 0*nul{k}.stat;
 end
+
+% W = weights{1};
+% W.stat(W.inside,:) = squeeze(squeeze(nanmean(nanmean(primal,2),1)));
+
 
 cfg=[];
 cfg.method = 'analytic';
-
 cfg.statistic = 'depsamplesT';
-
 cfg.correctm = 'no';
-cfg.parameter = 'primal';
+cfg.parameter = 'stat';
 cfg.ivar = 1;
 cfg.uvar = 2;
-cfg.design = [ones(1,nsign) 2*ones(1,nsign); 1:nsign, 1:nsign];
+cfg.design = [ones(1,n) 2*ones(1,n); 1:n, 1:n];
+stat = ft_sourcestatistics(cfg, weights{:}, nul{:});
 
-% cfg.method = 'montecarlo';
-% cfg.numrandomization = 1000;
-% cfg.correctm = 'cluster';
-stat = ft_sourcestatistics(cfg, diff2{:}, nul{:})
-
-cfg2 = [];
-% cfg2.frequency = 10;
-cfg2.parameter = {'stat', 'mask'};
-stat_int = ft_sourceinterpolate(cfg2, stat, mri)
-
-
-
-
-
-
+cfgp=[];
+cfgp.funparameter = 'stat';
+cmap = flipud(brewermap(64,'RdBu'));
+cfgp.funcolormap = cmap;
+cfgp.maskparameter = 'stat';
+cfgp.method = 'slice';
+for k=freqs
+  cfgp.frequency=k;
+  ft_sourceplot(cfgp, stat);
+end
