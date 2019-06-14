b96fa09cee728933a5a8072e32ed3e04e4056ba9
Mats van Es <m.vanes@donders.ru.nl>
2019-06-14 17:26:16 +0200

    pass on missing variable to next function.

--- matlab/vismot_bf_coh_roi_script.m
+++ matlab/vismot_bf_coh_roi_script.m
@@ -73,7 +73,7 @@ end
 
 subject = vismot_subjinfo(subjectname);
 load(fullfile(subject.pathname,'grid',sprintf('%s_sourcemodel3d4mm',subject.name)),'sourcemodel');
-[coh,zx13,zx42,looptime] = vismot_bf_coh_roi(subject,'toi', toi,'conditions', conditions, 'sourcemodel',sourcemodel,'lambda', lambda, 'frequency',frequency,'smoothing',smoothing,'nrand',nrand, 'ref', ref, 'include_neighb', include_neighb);
+[coh,zx13,zx42,looptime] = vismot_bf_coh_roi(subject,'toi', toi,'conditions', conditions, 'sourcemodel',sourcemodel,'lambda', lambda, 'frequency',frequency,'smoothing',smoothing,'nrand',nrand, 'ref', ref, 'roi_to', roi_to,'include_neighb', include_neighb);
 
 filename = fullfile(subject.pathname,'source', [subject.name, '_coh6d4mm_', sprintf('%s_', toi), sprintf('roi2%s_', roi_to),sprintf('%03d', frequency)] );
 if include_neighb
