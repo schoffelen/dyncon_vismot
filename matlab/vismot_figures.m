d2c909f0b47441cf3393451acb66bbd41a2faee2
Mats van Es <m.vanes@donders.ru.nl>
2019-06-25 11:29:36 +0200

    choose figures for chapter and adapt them, clean up the rest.

--- matlab/vismot_figures.m
+++ matlab/vismot_figures.m
@@ -92,85 +92,35 @@ end
 
 
 %% FIGURE 4 - post cue power ROI + bar plot
-% Make bar graph of defined of power in defined ROIs and FOIs
-% find indices of roi locations
+% raincloud graph of ipsi vs contra ROI pow diff (C./IC)
+idx_left = d2.l;
+idx_right = d2.r;
 
-idx_left = d.l;
-idx_right = d.r;
+pow_ipsi = zeros(19,15);
+pow_contra = zeros(19,15);
+zeroidx = [2 7 14];
+pow_ipsi(:, setdiff(1:15, zeroidx)) = d2.effectsize_roi_left;
+pow_contra(:, setdiff(1:15, zeroidx))  = d2.effectsize_roi_right;
 
-cmap = brewermap(5, 'Accent');
-
+cmap = brewermap(2, 'RdBu');
 figure;
-% occipital
-subplot(1,3,1);
-occ = [mean(d.effectsize_roi_left(:,1),1), mean(d.effectsize_roi_left(:,2),1) mean(d.effectsize_roi_left(:,3),1), mean(d.effectsize_roi_left(:,4),1); mean(d.effectsize_roi_right(:,1),1), mean(d.effectsize_roi_right(:,2),1) mean(d.effectsize_roi_right(:,3),1), mean(d.effectsize_roi_right(:,4),1)];
-% occ = [d.stat.stat(idx_left(1),1), d.stat.stat(idx_left(2),3) d.stat.stat(idx_left(3),4), d.stat.stat(idx_left(4),5); d.stat.stat(idx_right(1),1), d.stat.stat(idx_right(2),3) d.stat.stat(idx_right(3),4), d.stat.stat(idx_right(4),5)];
-b = bar(occ);
-tmpcmap = cmap([1 3:5],:);
-for k=1:4
-  set(b(k),'FaceColor', tmpcmap(k,:));
-end
-set(gca,'xticklabel',{'left', 'right'});
-legend({'alpha', 'gamma1', 'gamma2', 'gamma3'}, 'location', 'northwest')
-title('occipital')
-
-% parietal
-subplot(1,3,2);
-par = [mean(d.effectsize_roi_left(:,5),1), mean(d.effectsize_roi_left(:,6),1) mean(d.effectsize_roi_left(:,7),1), mean(d.effectsize_roi_left(:,8),1); mean(d.effectsize_roi_right(:,5),1), mean(d.effectsize_roi_right(:,6),1) mean(d.effectsize_roi_right(:,7),1), mean(d.effectsize_roi_right(:,8),1)];
-% par = [stat.stat(idx_left(5),1), stat.stat(idx_left(6),3) stat.stat(idx_left(7),4), stat.stat(idx_left(8),5); stat.stat(idx_right(5),1), stat.stat(idx_right(6),3) stat.stat(idx_right(7),4), stat.stat(idx_right(8),5)];
-b = bar(par);
-tmpcmap = cmap([1 3:5],:);
-for k=1:4
-  set(b(k),'FaceColor', tmpcmap(k,:));
-end
-set(gca,'xticklabel',{'left', 'right'});
-legend({'alpha', 'gamma1', 'gamma2', 'gamma3'}, 'location', 'northwest')
-title('parietal')
-
-% motor
-subplot(1,3,3);
-% mot = [stat.stat(idx_left(9),1), stat.stat(idx_left(10),2) stat.stat(idx_left(11),3), stat.stat(idx_left(12),5); stat.stat(idx_right(9),1), stat.stat(idx_right(10),2) stat.stat(idx_right(11),3), stat.stat(idx_right(12),5)];
-mot = [mean(d.effectsize_roi_left(:,9),1), mean(d.effectsize_roi_left(:,10),1) mean(d.effectsize_roi_left(:,11),1), mean(d.effectsize_roi_left(:,12),1); mean(d.effectsize_roi_right(:,9),1), mean(d.effectsize_roi_right(:,10),1) mean(d.effectsize_roi_right(:,11),1), mean(d.effectsize_roi_right(:,12),1)];
-
-b = bar(mot);
-tmpcmap = cmap([1:3 5],:);
-for k=1:4
-  set(b(k),'FaceColor', tmpcmap(k,:));
-end
-set(gca,'xticklabel',{'left', 'right'});
-legend({'alpha', 'beta', 'gamma1', 'gamma3'}, 'location', 'northeast')
-title('motor')
-
-%% or per frequency
-% Make bar graph of defined of power in defined ROIs and FOIs
-% find indices of roi locations
-for k=1:size(roi,1)-1
-    idx_left(k) = find_dipoleindex(d2.stat, roi{k+1,3});
-    idx_right(k) = find_dipoleindex(d2.stat, roi{k+1,4});
-end
-
-
-cmap = brewermap(3, 'Accent');
-freqs = {'8-12', '14-30', '30-50', '50-70', '70-90'};
-
-figure;
-for f=1:5
-subplot(1,5,f);
-a = [stat.stat(idx_left(1),f), stat.stat(idx_left(5),f) stat.stat(idx_left(9),f); stat.stat(idx_right(1),f), stat.stat(idx_right(5),f) stat.stat(idx_right(9),f)];
-b = bar(a);
-for k=1:3
-  set(b(k),'FaceColor', cmap(k,:));
-end
-set(gca,'xticklabel',{'left', 'right'});
-if f==1; legend({'occipital', 'parietal', 'motor'}), end
-title(sprintf('%s Hz', freqs{f}))
+for k = setdiff(1:15, zeroidx)
+    subplot(3,5,k)
+  h1{k} = raincloud_plot(pow_ipsi(:,k), 'box_on', 1, 'color', cmap(1,:), 'alpha', 0.5,...
+     'box_dodge', 1, 'box_dodge_amount', .15, 'dot_dodge_amount', .15,...
+     'box_col_match', 0);
+  h2{k} = raincloud_plot(pow_contra(:,k), 'box_on', 1, 'color', cmap(2,:), 'alpha', 0.5,...
+     'box_dodge', 1, 'box_dodge_amount', .35, 'dot_dodge_amount', .35, 'box_col_match', 0);
+f1 = ksdensity(pow_ipsi(:,k));
+f2 = ksdensity(pow_contra(:,k));
+   maxy = max([f1 f2]);
+   set(gca, 'Ylim', [-maxy maxy])
 end
 
 % ROI plot
-s = stat;
-s.stat = s.stat(:,1);
+s = d2.stat;
+s.stat = d2.stat.stat(:,1);
 s.freq = 1;
-load('/project/3011085.03/analysis/stat_bf_pre.mat', 'l', 'r');
 cmap = brewermap(3, 'Accent');
 location = {'L-occipital', 'R-occipital'; 'L-parietal', 'R-parietal'; 'L-motor', 'R-motor'};
 
@@ -208,11 +158,11 @@ p = [10];
 grad=[];
 grad.label = {'1'};
 for k=1:length(p)
-  grad.chanpos=[s.pos(l(p(k)),:)]+[0 0 2];
+  grad.chanpos=[s.pos(l(p(k)),:)]+[0 0 1.5];
   grad.elecpos=grad.chanpos;
   ft_plot_sens(grad,'elecshape','point', 'elecsize', 40, 'facecolor', [cmap(3,:)])
   
-  grad.chanpos=[s.pos(r(p(k)),:)]+[0 0 2];
+  grad.chanpos=[s.pos(r(p(k)),:)]+[0 0 1.5];
   grad.elecpos=grad.chanpos;
   ft_plot_sens(grad,'elecshape','point', 'elecsize', 40, 'facecolor', [cmap(3,:)])
 end
@@ -227,10 +177,16 @@ grad=[];
 grad.label = {'1'};
 for k=1:length(p)
   grad.chanpos=[s.pos(l(p(k)),:)];
+  if k==3
+    grad.chanpos = grad.chanpos + [0 0 2];
+  end
   grad.elecpos=grad.chanpos;
   ft_plot_sens(grad,'elecshape','point', 'elecsize', 40, 'facecolor', [cmap(k,:)])
   
   grad.chanpos=[s.pos(r(p(k)),:)];
+    if k==3
+    grad.chanpos = grad.chanpos + [0 0 2];
+  end
   grad.elecpos=grad.chanpos;
   ft_plot_sens(grad,'elecshape','point', 'elecsize', 40, 'facecolor', [cmap(k,:)])
 end
@@ -245,10 +201,16 @@ grad=[];
 grad.label = {'1'};
 for k=1:length(p)
   grad.chanpos=[s.pos(l(p(k)),:)];
+  if k==1
+    grad.chanpos = grad.chanpos - [0 1 0];
+  end
   grad.elecpos=grad.chanpos;
   ft_plot_sens(grad,'elecshape','point', 'elecsize', 40, 'facecolor', [cmap(k,:)])
   
   grad.chanpos=[s.pos(r(p(k)),:)];
+  if k==1
+    grad.chanpos = grad.chanpos - [0 1.2 0];
+  end
   grad.elecpos=grad.chanpos;
   ft_plot_sens(grad,'elecshape','point', 'elecsize', 40, 'facecolor', [cmap(k,:)])
 end
@@ -276,86 +238,59 @@ view([-90,0]);camlight
 %% pre cue power
 alldir = '/project/3011085.03/';
 s = load(fullfile([alldir, 'analysis/stat_bf_pre.mat']));
-% OPTION 1
-cmap = brewermap(2, 'RdBu');
-figure;
-for k=1:12
-d{k,1} = s.lpow(:,k);
-d{k,2} = s.rpow(:,k);
-end
-h = rm_raincloud(d, cmap);
-for k=1:numel(h.l)
-  h.l(k).Color = 'none';
-end
-for k=1:numel(h.m)
-    h.m(k).MarkerFaceColor = 'none';
-  h.m(k).MarkerEdgeColor = 'none';
-end
 
-% OPTION 2
+pow_ipsi = zeros(19,15);
+pow_contra = zeros(19,15);
+zeroidx = [2 7 14];
+pow_ipsi(:, setdiff(1:15, zeroidx)) = s.effectsize_roi_left;
+pow_contra(:, setdiff(1:15, zeroidx))  = s.effectsize_roi_right;
+
 figure;
-cmap2 = brewermap(2, 'RdBu');
-for k=1:size(s.lpow,2)
-  subplot(3,4,k)
-  h1{k} = raincloud_plot(s.lpow(:,k), 'box_on', 1, 'color', cmap(1,:), 'alpha', 0.5,...
+cmap = brewermap(2, 'RdBu');
+for k=setdiff(1:15, zeroidx)
+  subplot(3,5,k)
+  h1{k} = raincloud_plot(pow_ipsi(:,k), 'box_on', 1, 'color', cmap(1,:), 'alpha', 0.5,...
      'box_dodge', 1, 'box_dodge_amount', .15, 'dot_dodge_amount', .15,...
      'box_col_match', 0);
-  h2{k} = raincloud_plot(s.rpow(:,k), 'box_on', 1, 'color', cmap(2,:), 'alpha', 0.5,...
+  h2{k} = raincloud_plot(pow_contra(:,k), 'box_on', 1, 'color', cmap(2,:), 'alpha', 0.5,...
      'box_dodge', 1, 'box_dodge_amount', .35, 'dot_dodge_amount', .35, 'box_col_match', 0);
+   f1 = ksdensity(pow_ipsi(:,k));
+f2 = ksdensity(pow_contra(:,k));
+   maxy = max([f1 f2]);
+   set(gca, 'Ylim', [-maxy maxy])
 end
 
-% OPTION 3
-  diff = s.lpow-s.rpow;
-  addpath /project/3011085.03/scripts/IoSR-Surrey-MatlabToolbox-4bff1bb/
-figure; iosr.statistics.boxPlot(diff', 'showScatter', true, 'scatterMarker', '.'); 
-load(fullfile([alldir, 'analysis/source/roi.mat']));
-cmap = brewermap(5, 'Accent');
-for k=1:12
-%   c{k} = cmap(s.freq_idx(k),:);
-x{1}(k,:) = [cmap(s.freq_idx(k),:)];
-end
-
-% OPTION 4
-meanl = mean(s.lpow,1);
-meanr = mean(s.rpow,1);
-figure;
-tmpcmap = cmap([1 3:5],:);
-subplot(1,3,1)
-b = bar([s.lpow(1:4); s.rpow(1:4)]);
-for k=1:4
-  set(b(k),'FaceColor', tmpcmap(k,:));
-end
-set(gca,'xticklabel',{'left', 'right'});
-subplot(1,3,2)
-b = bar([s.lpow(5:8); s.rpow(5:8)]);
-for k=1:4
-  set(b(k),'FaceColor', tmpcmap(k,:));
-end
-set(gca,'xticklabel',{'left', 'right'});
-subplot(1,3,3)
-tmpcmap = cmap([1:3 5],:);
-b = bar([s.lpow(9:12); s.rpow(9:12)]);
-for k=1:4
-  set(b(k),'FaceColor', tmpcmap(k,:));
-end
-set(gca,'xticklabel',{'left', 'right'});
 
 
 %% Coherence
 % within-between hemispheres
-c = load('/project/3011085.03/analysis/stat_coh_pre.mat')
+c = load('/project/3011085.03/analysis/stat_coh_pre.mat');
 
-cmap = flipud(brewermap(2,'RdBu'));
-f1 = figure;
-h1 = raincloud_plot(c.zx_within, 'box_on', 1, 'color', cmap(1,:), 'alpha', 0.5,...
+cmap = brewermap(2,'RdBu');
+figure;
+subplot(1,2,1)
+h1 = raincloud_plot(c.coh_within_C, 'box_on', 1, 'color', cmap(1,:), 'alpha', 0.5,...
      'box_dodge', 1, 'box_dodge_amount', .15, 'dot_dodge_amount', .15,...
      'box_col_match', 0);
-h2 = raincloud_plot(c.zx_between, 'box_on', 1, 'color', cmap(2,:), 'alpha', 0.5,...
+h2 = raincloud_plot(c.coh_within_IC, 'box_on', 1, 'color', cmap(2,:), 'alpha', 0.5,...
      'box_dodge', 1, 'box_dodge_amount', .35, 'dot_dodge_amount', .35, 'box_col_match', 0);
-legend([h1{1} h2{1}], {'Intrahemispheric connectivity', 'Interhemispheric connectivity'});
-title('Coherence within vs between hemispheres');
-set(gca,'Xlim', [-0.03 0.03], 'Ylim', [-30 60]);
-xlabel('Coherence congruent-incongruent (T)'); ylabel('Probability (a.u.)')
+legend([h1{1} h2{1}], {'Previous congruent', 'Previous incongruent'});
+title('Coherence within hemispheres');
+set(gca,'Xlim', [0.05 0.3], 'Ylim', [-12 25]);
+xlabel('Coherence'); ylabel('Probability (coh^-1)')
+box off
+
+
+subplot(1,2,2)
+h3 = raincloud_plot(c.coh_between_C, 'box_on', 1, 'color', cmap(1,:), 'alpha', 0.5,...
+     'box_dodge', 1, 'box_dodge_amount', .15, 'dot_dodge_amount', .15,...
+     'box_col_match', 0);
+h4 = raincloud_plot(c.coh_between_IC, 'box_on', 1, 'color', cmap(2,:), 'alpha', 0.5,...
+     'box_dodge', 1, 'box_dodge_amount', .35, 'dot_dodge_amount', .35, 'box_col_match', 0);
+legend([h1{1} h2{1}], {'Previous congruent', 'Previous incongruent'});
+title('Coherence between hemispheres');
+set(gca,'Xlim', [0.02 0.2], 'Ylim', [-10 20]);
+xlabel('Coherence'); ylabel('Probability (coh^-1)')
 box off
 
 % individual coherences
@@ -406,48 +341,14 @@ C.cohspctrm = coh;
 C.dimord = 'chan_chan_freq';
 C.freq=[10 22 40 60 80];
 
-% FT way
-% make a parameter containing width of the arrows (in pixels) according to
-% the coh value
-C.width = round(2*C.cohspctrm);
-h = figure;
-viscircles([0,0], 1, 'color','k')
-load('vismot_parcels', 'layout3', 'plotlayoptions3');
-layout3.label = C.label;
-layout3.pos = [layout3.pos(4:6,:); layout3.pos(1:3,:)];
-% plotlayoptions3{10} = [plotlayoptions3{10}(4:6); plotlayoptions3{10}(1:3)];
-plotlayoptions3{8}=zeros(1,6);
-
-cfgp=[];
-cfgp.layout=layout3;
-cfgp.widthparam = 'width';
-cfgp.newfigure = 'no';
-cfgp.colormap = flipud(brewermap(2, 'RdBu'));
-for k=1:numel(C.freq)
-  figure; hold on; 
-%   viscircles([0,0], 1, 'color','k')
-  cfgp.colormap = cmap(k,:);
-  cfgp.foi = C.freq(k);
-  ft_topoplotCC(cfgp, C);
-  ft_plot_layout(layout3, plotlayoptions3{:});
-end
-% ft_plot_layout(layout3, plotlayoptions3{:});
-
-
-% Circular Graph
-label = {'L_occ', 'L_par', 'L_mot','R_occ', 'R_par', 'R_mot'};
-cmap = brewermap(5, 'Accent');
-addpath('/project/3011085.03/scripts/circularGraph/');
-for k=1:5
-  figure;
-  viscircles([0,0], 1, 'color','k'); hold on
-  circularGraph(C.cohspctrm(:,:,k), 'Colormap', repmat(cmap(k,:), [6 1]), 'Label', label);
-end
-
-% or 
 label = {'L_occ', 'L_par', 'L_mot','R_occ', 'R_par', 'R_mot'};
 cmap = brewermap(2, 'RdBu');
 addpath('/project/3011085.03/scripts/circularGraph/');
+% permute data and labels such that the graph is symmetrical and in the
+% right order
+order = [2 1 4 5 6 3];
+label = label(order);
+C.cohspctrm = C.cohspctrm(order, order, :);
 for k=1:5
   figure;
   viscircles([0,0], 1, 'color','k'); hold on
@@ -458,8 +359,3 @@ for k=1:5
   tmpc(tmpc>0) = 0;
   circularGraph(abs(tmpc), 'Colormap', repmat(cmap(2,:), [6 1]), 'Label', label);
 end
-
-
-
-
-% legend({'alpha', 'beta', 'gamma1', 'gamma2', 'gamma3'}) % doesnt work
