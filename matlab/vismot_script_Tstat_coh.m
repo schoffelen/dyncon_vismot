36d938349dd4ea728bbe80c0b8f2fe4c66a738d4
Mats van Es <m.vanes@donders.ru.nl>
2019-06-25 11:27:16 +0200

    do statistics on within and between hemispheres seperately (C-IC).

--- matlab/vismot_script_Tstat_coh.m
+++ matlab/vismot_script_Tstat_coh.m
@@ -20,6 +20,10 @@ for k=2:size(foi,1)
     sz = numel(tmpfreqs);
     tmpzx13 = [];
     tmpzx42 = [];
+    tmpcoh1 = [];
+    tmpcoh2 = [];
+    tmpcoh3 = [];
+    tmpcoh4 = [];
     for f = 1:sz
       
       filename = fullfile([datadir, sprintf('%s_coh6d4mm_%s_roi2%s_%03d', list{subj},toi,roi_to, tmpfreqs(f))]);
@@ -27,9 +31,17 @@ for k=2:size(foi,1)
       
       tmpzx13(:,:,f) = dum.zx13;
       tmpzx42(:,:,f) = dum.zx42;
+      tmpcoh1(:,:,f) = abs(dum.coh(1).coh);
+      tmpcoh2(:,:,f) = abs(dum.coh(2).coh);
+      tmpcoh3(:,:,f) = abs(dum.coh(3).coh);
+      tmpcoh4(:,:,f) = abs(dum.coh(4).coh);
     end
     zx13{cnt}(subj,:,:) = nanmean(tmpzx13,3);
     zx42{cnt}(subj,:,:) = nanmean(tmpzx42,3);
+    coh1{cnt}(subj,:,:) = nanmean(tmpcoh1,3);
+    coh2{cnt}(subj,:,:) = nanmean(tmpcoh2,3);
+    coh3{cnt}(subj,:,:) = nanmean(tmpcoh3,3);
+    coh4{cnt}(subj,:,:) = nanmean(tmpcoh4,3);
   end
   cnt=cnt+1;
 end
@@ -58,6 +70,31 @@ for k=1:numel(zx13)
     zx42_between(subj,k) = nanmean(nanmean(x42tmp(nloc+1:2*nloc, 1:nloc)));
     zx42_within(subj,k) = nanmean([nanmean(nanmean(x42tmp(1:nloc, 1:nloc))),...
       nanmean(nanmean(x42tmp(nloc+1:2*nloc,nloc+1:2*nloc)))]);
+    
+    x1tmp = squeeze(coh1{k}(subj,:,:));
+    x1tmp(find(triu(x))) = nan;
+    coh1_between(subj,k) = nanmean(nanmean(x1tmp(nloc+1:2*nloc, 1:nloc)));
+    coh1_within(subj,k) = nanmean([nanmean(nanmean(x1tmp(1:nloc, 1:nloc))),...
+      nanmean(nanmean(x1tmp(nloc+1:2*nloc,nloc+1:2*nloc)))]);
+    
+        x2tmp = squeeze(coh2{k}(subj,:,:));
+    x2tmp(find(triu(x))) = nan;
+    coh2_between(subj,k) = nanmean(nanmean(x2tmp(nloc+1:2*nloc, 1:nloc)));
+    coh2_within(subj,k) = nanmean([nanmean(nanmean(x2tmp(1:nloc, 1:nloc))),...
+      nanmean(nanmean(x2tmp(nloc+1:2*nloc,nloc+1:2*nloc)))]);
+    
+        x3tmp = squeeze(coh3{k}(subj,:,:));
+    x3tmp(find(triu(x))) = nan;
+    coh3_between(subj,k) = nanmean(nanmean(x3tmp(nloc+1:2*nloc, 1:nloc)));
+    coh3_within(subj,k) = nanmean([nanmean(nanmean(x3tmp(1:nloc, 1:nloc))),...
+      nanmean(nanmean(x3tmp(nloc+1:2*nloc,nloc+1:2*nloc)))]);
+    
+        x4tmp = squeeze(coh4{k}(subj,:,:));
+    x4tmp(find(triu(x))) = nan;
+    coh4_between(subj,k) = nanmean(nanmean(x4tmp(nloc+1:2*nloc, 1:nloc)));
+    coh4_within(subj,k) = nanmean([nanmean(nanmean(x4tmp(1:nloc, 1:nloc))),...
+      nanmean(nanmean(x4tmp(nloc+1:2*nloc,nloc+1:2*nloc)))]);
+    
   end
 end
 
@@ -65,15 +102,33 @@ end
 zx_between = (zx13_between + zx42_between)./2;
 zx_within = (zx13_within + zx42_within)./2;
 
+% pool conditions 1+4 and 1+3
+coh_between_C = (coh1_between + coh4_between)./2;
+coh_between_IC = (coh3_between + coh2_between)./2;
+coh_within_C = (coh1_within + coh4_within)./2;
+coh_within_IC = (coh3_within + coh2_within)./2;
+
 % average over frequencies
 zx_between = nanmean(zx_between,2);
 zx_within = nanmean(zx_within,2);
+coh_between_C = nanmean(coh_between_C, 2);
+coh_between_IC = nanmean(coh_between_IC, 2);
+coh_within_C = nanmean(coh_within_C, 2);
+coh_within_IC = nanmean(coh_within_IC, 2);
+
 
 % we expect within hemisphere connectivity to be higher for conditions 1-3
 % and 4-2, w.r.t. between hemispheres connectivity: i.e.
 % zx_within>zxbetween
 [w.H,w.P,w.CI,w.STATS] = ttest(zx_within, zx_between, 'tail', 'right');
 within_vs_between_hemiC = w;
+
+[w.H,w.P,w.CI,w.STATS] = ttest(coh_within_C, coh_within_IC, 'tail', 'right');
+within_CvsIC = w;
+
+[w.H,w.P,w.CI,w.STATS] = ttest(coh_between_C, coh_between_IC, 'tail', 'left');
+between_CvsIC = w;
+
 
 %% Is there any connectivity pattern significant individually (i.e. larger/smaller for C than IC)?
 % combine 1-3 and 4-2
@@ -124,7 +179,7 @@ cfgs.numrandomization = 10000;
 cfgs.correcttail = 'prob';
 stat = ft_timelockstatistics(cfgs, data, nul);
 
-save(fullfile([alldir, 'analysis/stat_coh_pre.mat']), 'stat', 'allconnectivity', 'data', 'ncomparisson', 'zx', 'within_vs_between_hemiC', 'zx_within', 'zx_between');
+save(fullfile([alldir, 'analysis/stat_coh_pre.mat']), 'stat', 'allconnectivity', 'data', 'ncomparisson', 'zx', 'within_vs_between_hemiC', 'zx_within', 'zx_between', 'within_CvsIC', 'between_CvsIC','coh_within_C', 'coh_within_IC', 'coh_between_C', 'coh_between_IC');
 
 
 %{
