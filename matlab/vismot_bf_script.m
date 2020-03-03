4a17f87e20facea6434c2cd406b1848b2882d4c1
Mats van Es <m.vanes@donders.ru.nl>
2020-03-03 17:07:53 +0100

    repair faulty filename.

--- matlab/vismot_bf_script.m
+++ matlab/vismot_bf_script.m
@@ -20,7 +20,7 @@ load(fullfile(subject.pathname,'grid',sprintf('%s_sourcemodel3d4mm',subject.name
 [source, stat, filter, freqpow] = vismot_bf(subject,'frequency',frequency, 'smoothing', smoothing, 'sourcemodel',sourcemodel,'prewhiten',prewhiten, 'lambda', lambda, 'nrand', nrand, 'latoi', latoi, 'stratifyflag', stratifyflag);
 
 %% post process stats
-filename = fullfile(subject.pathname,'source','tmp', [subject.name,sprintf('_source3d4mm_%s_', latoi), num2str(frequency,'%03d')]);
+filename = fullfile(subject.pathname,'source', [subject.name,sprintf('_source3d4mm_%s_', latoi), num2str(frequency,'%03d')]);
 if istrue(prewhiten), filename = [filename '_prewhitened']; end
 if istrue(stratifyflag), filename = [filename, '_stratified']; end
 if nrand>0, filename = [filename, '_resamp']; end
