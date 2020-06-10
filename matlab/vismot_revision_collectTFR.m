94ed38cbe14c6682b4165a61c139b90f11bec9ef
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2020-06-10 11:39:42 +0200

    ENH - updated plotting script

--- matlab/vismot_revision_collectTFR.m
+++ matlab/vismot_revision_collectTFR.m
@@ -65,10 +65,12 @@ end
 
 doplot = true;
 if doplot
+  addpath ~/matlab/toolboxes/export_fig/
+  
   load gaTFR_high
   
   cfg = [];
-  cfg.frequency = [34 100];
+  cfg.frequency = [38 100];
   cfg.latency   = [-0.4 0.5];
   Favg_high = ft_selectdata(cfg, Favg);
 
@@ -78,9 +80,79 @@ if doplot
   cfg = [];
   cfg.colormap = '*RdBu'; % this requires the currently open PR of JM's colormap branch
   cfg.zlim = 'maxabs';
+  cfg.xlim = [0.2 inf];
   cfg.layout = '4D248_helmet.mat';
   cfg.gridscale = 150;
-  ft_topoplotTFR(cfg, Favg_high);
+  figure;ft_topoplotTFR(cfg, Favg_high);
+  export_fig('-png','-eps','m2', 'tfr_topo_high_allfreq');
+  cfg.ylim = [34 50];
+  cfg.highlightchannel = {'A140' 'A167' 'A168' 'A189'};
+  cfg.highlightcolor = [0 0 0];
+  cfg.highlightsize = 24;
+  cfg.highlightsymbol = '.';
+  cfg.highlight = 'on';
+  figure;ft_topoplotTFR(cfg, Favg_high);
+  export_fig('-png','-eps','m2', 'tfr_topo_high_lowgamma');
+  cfg.ylim = [54 66];
+  cfg.highlightchannel = {'A134' 'A161' 'A162' 'A183'};
+  cfg.highlightcolor = [0 0 0];
+  figure;ft_topoplotTFR(cfg, Favg_high);
+  export_fig('-png','-eps','m2', 'tfr_topo_high_midgamma');
+  cfg.ylim = [74 86];
+  cfg.highlightchannel = {'A8' 'A9' 'A23' 'A24'};
+  cfg.highlightcolor = [1 1 1];
+  figure;ft_topoplotTFR(cfg, Favg_high);
+  export_fig('-png','-eps','m2', 'tfr_topo_high_highgamma');
+  cfg.ylim = [5.5 6.5];
+  cfg.xlim = [0.1 inf];
+  cfg.highlightchannel = {'A24' 'A25' 'A43' 'A44'};
+  cfg.highlightcolor = [0 0 0];
+  figure;ft_topoplotTFR(cfg, Favg_low);
+  export_fig('-png','-eps','m2', 'tfr_topo_low_theta');
+  cfg.ylim = [9.5 10.5];
+  cfg.highlightchannel = {'A188' 'A205' 'A206' 'A222'};
+  cfg.highlightcolor = [1 1 1];
+  figure;ft_topoplotTFR(cfg, Favg_low);
+  export_fig('-png','-eps','m2', 'tfr_topo_low_alpha');
+  cfg.ylim = [21.5 22.5];
+  cfg.highlightchannel = {'A25' 'A26' 'A44' 'A45'};
+  cfg.highlightcolor = [1 1 1];
+  figure;ft_topoplotTFR(cfg, Favg_low);
+  export_fig('-png','-eps','m2', 'tfr_topo_low_beta');
+  
+  cfg = rmfield(cfg, 'xlim');
+  cfg = rmfield(cfg, 'ylim');
+  
+  cfg.channel = {'A140' 'A167' 'A168' 'A189'};
+  cfg.title   = 'right occipito-parietal';
+  figure;ft_singleplotTFR(cfg, Favg_high);
+  export_fig('-png','-eps','m2', 'tfr_lowgamma');
+  
+  cfg.channel = {'A134' 'A161' 'A162' 'A183'};
+  cfg.title   = 'left occipito-parietal';
+  figure;ft_singleplotTFR(cfg, Favg_high);
+  export_fig('-png','-eps','m2', 'tfr_midgamma');
+  
+  cfg.channel = {'A8' 'A9' 'A23' 'A24'};
+  cfg.title   = 'central';
+  figure;ft_singleplotTFR(cfg, Favg_high);
+  export_fig('-png','-eps','m2', 'tfr_highgamma');
+  
+  cfg.channel = {'A24' 'A25' 'A43' 'A44'};
+  cfg.title   = 'central';
+  cfg.zlim = [-0.4 0.4];
+  figure;ft_singleplotTFR(cfg, Favg_low);
+  export_fig('-png','-eps','m2', 'tfr_theta');
+  
+  cfg.channel = {'A188' 'A205' 'A206' 'A222'};
+  cfg.title   = 'occipito-parietal';
+  figure;ft_singleplotTFR(cfg, Favg_low);
+  export_fig('-png','-eps','m2', 'tfr_alpha');
+  
+  cfg.channel = {'A25' 'A26' 'A44' 'A45'};
+  cfg.title   = 'sensorimotor';
+  figure;ft_singleplotTFR(cfg, Favg_low);
+  export_fig('-png','-eps','m2', 'tfr_beta');
   
   
 end
