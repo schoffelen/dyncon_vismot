d9624e2314c64b5d2ba4895f6b965aa35f279b88
Mats van Es <m.vanes@donders.ru.nl>
2021-01-04 11:50:22 +0100

    anonymize subject names

--- matlab/vismot_subjinfo.m
+++ matlab/vismot_subjinfo.m
@@ -1,11 +1,11 @@
 function subject = vismot_subjinfo(subjectname)
 
 if nargin<1
-  subjectname = {'AHE08';'AMN20';'BKA01';'CBE22';...
-                 'CDE04';'DBD12';'GAR12';'JGN27';...
-                 'JOE22';'LCE09';'MAI27';'MHH14';...
-                 'MME25';'PCL19';'RBE13';'SZQ02';...
-                 'T001';'TMR04';'VIA12'};
+  subjectname = {'sub01';'sub02';'sub03';'sub04';...
+                 'sub05';'sub06';'sub07';'sub08';...
+                 'sub09';'sub10';'sub11';'sub12';...
+                 'sub13';'sub14';'sub15';'sub16';...
+                 'sub17';'sub18';'sub19'};
 end
 
 if strcmp(computer, 'PCWIN')
@@ -29,8 +29,8 @@ end
 
 for k = 1:numel(subjectname)
   switch subjectname{k}
-    case 'JOE22'
-      subject(k).name        = 'JOE22';
+    case 'sub09'
+      subject(k).name        = 'sub09';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'Seated1000/';
       subject(k).sessionname = '09-01-30@1414/';
@@ -40,8 +40,8 @@ for k = 1:numel(subjectname)
       subject(k).datafile    = 'c,rfDC';
       subject(k).denoise     = 0;
       subject(k).congrROIfoi = [48 64; 40 52];
-    case 'T001'
-      subject(k).name        = 'T001';
+    case 'sub17'
+      subject(k).name        = 'sub17';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'Seated1000/';
       subject(k).sessionname = '09-02-05@1014/';
@@ -52,8 +52,8 @@ for k = 1:numel(subjectname)
       subject(k).mriname     = 'GGA04';
       subject(k).denoise     = 0;
       subject(k).congrROIfoi = [36 64; 84 96];
-    case 'MHH14'
-      subject(k).name        = 'MHH14';
+    case 'sub12'
+      subject(k).name        = 'sub12';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'SVeNoeeg10/';
       subject(k).sessionname = '09-02-19@1225/';
@@ -73,8 +73,8 @@ for k = 1:numel(subjectname)
       subject(k).pathname    = pana;
       subject(k).datafile    = 'hc,rfDC';
       subject(k).denoise     = 1;
-    case 'SZQ02'
-      subject(k).name        = 'SZQ02';
+    case 'sub16'
+      subject(k).name        = 'sub16';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'Seated1Khm/';
       subject(k).sessionname = '09-03-06@1409/';
@@ -85,8 +85,8 @@ for k = 1:numel(subjectname)
       %subject(k).badchannels = {'-A107';'-A121';'-A40';'-A146';'-A248'};
       subject(k).denoise     = 1;
       subject(k).congrROIfoi = [20 20; 60 80];
-    case 'JGN27'
-      subject(k).name        = 'JGN27';
+    case 'sub08'
+      subject(k).name        = 'sub08';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1kchmt/';
       subject(k).sessionname = '09-03-10@1105/';
@@ -97,8 +97,8 @@ for k = 1:numel(subjectname)
       %subject(k).badchannels = {'-A107';'-A40';'-A248'};
       subject(k).denoise     = 1;
       subject(k).congrROIfoi = [64 88; 72 92];
-    case 'GAR12'
-      subject(k).name        = 'GAR12';
+    case 'sub07'
+      subject(k).name        = 'sub07';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1k@6Ehmt/';
       subject(k).sessionname = '09-03-17@1042/';
@@ -108,8 +108,8 @@ for k = 1:numel(subjectname)
       subject(k).datafile    = 'hc,rfDC';
       subject(k).denoise     = 1;
       subject(k).cohname     = 'Std1k@6EEG/09-03-17@1031/1/';
-    case 'MME25'
-      subject(k).name        = 'MME25';
+    case 'sub13'
+      subject(k).name        = 'sub13';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1k@6Ehmt/';
       subject(k).sessionname = '09-03-18@1634/';
@@ -120,8 +120,8 @@ for k = 1:numel(subjectname)
       subject(k).denoise     = 1;
       subject(k).cohname     = 'Std1k@6EEG/09-03-18@1621/1/';
       subject(k).congrROIfoi = [60 80; 60 80];
-    case 'RBE13'
-      subject(k).name        = 'RBE13';
+    case 'sub15'
+      subject(k).name        = 'sub15';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1k@6Ehmt/';
       subject(k).sessionname = '09-03-19@1206/';
@@ -132,8 +132,8 @@ for k = 1:numel(subjectname)
       subject(k).denoise     = 1;
       subject(k).cohname     = 'Std1k@6EEG/09-03-19@1201/1/';
       subject(k).congrROIfoi = [60 80; 60 80];
-    case 'TMR04'
-      subject(k).name        = 'TMR04';
+    case 'sub18'
+      subject(k).name        = 'sub18';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1k@6Ehmt/';
       subject(k).sessionname = '09-03-19@1500/';
@@ -144,8 +144,8 @@ for k = 1:numel(subjectname)
       subject(k).denoise     = 1;
       subject(k).cohname     = 'Std1k@6EEG/09-03-19@1450/1/';
       subject(k).congrROIfoi = [52 60; 44 52];
-    case 'DBD12'
-      subject(k).name        = 'DBD12';
+    case 'sub06'
+      subject(k).name        = 'sub06';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1k@6Ehmt/';
       subject(k).sessionname = '09-03-20@1026/';
@@ -156,8 +156,8 @@ for k = 1:numel(subjectname)
       subject(k).denoise     = 1;
       subject(k).cohname     = 'Std1k@6EEG/09-03-20@0940/1/';
       subject(k).congrROIfoi = [40 60; 40 60];
-    case 'PCL19'
-      subject(k).name        = 'PCL19';
+    case 'sub14'
+      subject(k).name        = 'sub14';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1k@6Ehmt/';
       subject(k).sessionname = '09-03-20@1310/';
@@ -167,8 +167,8 @@ for k = 1:numel(subjectname)
       subject(k).datafile    = 'hc,rfDC';
       subject(k).denoise     = 1;
       subject(k).cohname     = 'Std1k@6EEG/09-03-20@1224/1/';
-    case 'AHE08'
-      subject(k).name        = 'AHE08';
+    case 'sub01'
+      subject(k).name        = 'sub01';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1k@6Ehmt/';
       subject(k).sessionname = '09-03-20@1605/';
@@ -179,8 +179,8 @@ for k = 1:numel(subjectname)
       subject(k).denoise     = 1;
       subject(k).cohname     = 'Std1k@6EEG/09-03-20@1524/1/';
       subject(k).congrROIfoi = [60 80; 60 80];
-    case 'VIA12'
-      subject(k).name        = 'VIA12';
+    case 'sub19'
+      subject(k).name        = 'sub19';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1k@6Ehmt/';
       subject(k).sessionname = '09-03-23@1201/';
@@ -191,8 +191,8 @@ for k = 1:numel(subjectname)
       subject(k).denoise     = 1;
       subject(k).cohname     = 'Std1k@6EEG/09-03-23@1112/1/';
       subject(k).congrROIfoi = [80 92; 68 88];
-    case 'MAI27'
-      subject(k).name        = 'MAI27';
+    case 'sub11'
+      subject(k).name        = 'sub11';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1k@6Ehmt/';
       subject(k).sessionname = '09-03-25@1338/';
@@ -203,8 +203,8 @@ for k = 1:numel(subjectname)
       subject(k).denoise     = 1;
       subject(k).cohname     = 'Std1k@6EEG/09-03-25@1334/1/';
       subject(k).congrROIfoi = [48 60; 60 80];
-    case 'CDE04'
-      subject(k).name        = 'CDE04';
+    case 'sub05'
+      subject(k).name        = 'sub05';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1k@6Ehmt/';
       subject(k).sessionname = '09-03-25@1603/';
@@ -215,8 +215,8 @@ for k = 1:numel(subjectname)
       subject(k).denoise     = 1;
       subject(k).cohname     = 'Std1k@6EEG/09-03-25@1510/1/';
       subject(k).congrROIfoi = [36 52; 36 56];
-    case 'BKA01'
-      subject(k).name        = 'BKA01';
+    case 'sub03'
+      subject(k).name        = 'sub03';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1k@6Ehmt/';
       subject(k).sessionname = '09-03-27@1613/';
@@ -226,8 +226,8 @@ for k = 1:numel(subjectname)
       subject(k).datafile    = 'hc,rfDC';
       subject(k).denoise     = 1;
       subject(k).cohname     = 'Std1k@6EEG/09-03-27@1533/1/';
-    case 'CBE22'
-      subject(k).name        = 'CBE22';
+    case 'sub04'
+      subject(k).name        = 'sub04';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1k@6Ehmt/';
       subject(k).sessionname = '09-03-31@1043/';
@@ -238,8 +238,8 @@ for k = 1:numel(subjectname)
       subject(k).denoise     = 1;
       subject(k).cohname     = 'Std1k@6EEG/09-03-31@1034/1/';
       subject(k).congrROIfoi = [40 60; 36 52];
-    case 'AMN20'
-      subject(k).name        = 'AMN20';
+    case 'sub02'
+      subject(k).name        = 'sub02';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1k@6Ehmt/';
       subject(k).sessionname = '09-04-01@1036/';
@@ -249,11 +249,11 @@ for k = 1:numel(subjectname)
       subject(k).datafile    = 'hc,rfDC';
       subject(k).denoise     = 1;
       subject(k).cohname     = 'Std1k@6EEG/09-04-01@1028/1/';
-      subject(k).ecgfile     = 'AMN20ecg-aligned.mat';
+      subject(k).ecgfile     = 'sub02ecg-aligned.mat';
       subject(k).ecgcomp     = [1 2 3];
       subject(k).congrROIfoi = [40 60; 64 80];
-    case 'LCE09'
-      subject(k).name        = 'LCE09';
+    case 'sub10'
+      subject(k).name        = 'sub10';
       subject(k).rawpath     = praw;
       subject(k).scanname    = 'S1k@6Ehmt/';
       subject(k).sessionname = '09-04-01@1311/';
